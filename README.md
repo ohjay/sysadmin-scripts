4e24f5f56acaa652cdc41fa72ffcd6e0f21ef0d4
Owen Jow <owenjow@berkeley.edu>
2015-07-22 23:44:09 -0700

    Wrote a script to clean up (i.e. delete) old files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # sysadmin-scripts
-Utility BASH scripts for system administration
+Utility BASH scripts for basic system administration. Once downloaded, a script in this repository can be run from Bash shells via the command ./[script-filepath] or via aliasing 
+
+## Options
+These scripts can be made into permanent commands (like `echo` or `grep`, for example) fairly easily. There are more than one ways to do this:
+
+- Create a $HOME/bin folder with `mkdir $HOME/bin`, and add the script to that folder (perhaps through `mv [script-filepath] $HOME/bin`). Put $HOME/bin on the PATH with the command `export PATH=$PATH:$HOME/bin`. If you want to execute the command à la builtin command (i.e. without the "sh" extension), feel free to delete aforementioned extension.
+- Alternatively, you can create an alias for the command. A temporary alias can be created with "alias command-name='./[script-filepath]', while a permanent alias will require the same command to be added to the ~/.bash_profile and/or ~/.bashrc file.
+
+##  Troubleshooting + FAQ
+- **I don't have permission to execute the file!** 
+
+⋅⋅⋅You can give yourself execute permission via the command `chmod +x [script-filepath]`.
---
+++ cleanup-old.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# cleanup-old
+# A utility script for removing old and unecessary files from secondary storage.
+# Any files in the current directory or any subdirectories may be considered.
+# Note: this is potentially a dangerous script, as it involves the deletion of files!
+# 
+# For every file that has not been modified for over 100 days, the user will be asked 
+# if he/she wants to remove that file from the system. If he/she says no, then he/she
+# will be given the option to ignore that file the next time this command is run.
+# 
+# $LastChangedDate: 2015-07-22 (Wed, 22 Jul 2015) $
+
+# confirm <prompt>
+# Prompts the user for a yes or no answer. If yes, this function returns 0 ("true").
+# If no, this function returns 1 ("false"). This function will continue to prompt
+# the user until it receives an answer of either yes or no.
+#
+# prompt is a string (likely a question) that will elicit the yes or no response
+function confirm {
+    eval prompt="$1"
+    while true; do
+        read -p "${prompt} " ans
+        case $ans in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please respond with either yes or no.";;
+        esac
+    done
+}
+
+# rm_file <file>
+# Asks the user whether or not he/she wants to remove the given file.
+# If so, this function will remove the file. Otherwise, the user will be given a chance
+# to add the file to the "ignore" list (i.e. list of files to  be ignored by this command).
+function rm_file {
+    eval file="$1"
+    
+    # Set the prompt strings
+    rm_prompt=$'\n'"${file}"$' has not been modified for over 100 days.\nDo you want to remove '"${file}"'?'
+    ignore_prompt="Do you want to add ${file} to the ignore list?"
+    
+    if confirm "\${rm_prompt}"; then
+        rm "${file}"
+    elif confirm "\${ignore_prompt}"; then
+        echo "${file}" >> ~/.cleanup-ignore.txt
+    fi
+}
+
+# check_files <filepath> <num_days>
+# Considers all old files (i.e. files that have not been accessed for > 100 days) 
+# on the given filepath for removal. (If a directory is empty, the FIND commands will complain.)
+function check_files {
+    eval filepath="$1"
+    local num_days=$1
+    
+    while IFS= read -u 3 -r -d '' file; do
+        # Check that the file is not in the ignore list
+        if ! grep -Fxq "${file}" ~/.cleanup-ignore.txt; then
+            rm_file "\${file}"
+        fi
+    done 3< <(find "${filepath}"* -maxdepth 0 -mmin +$((60 * 24 * 100)) -print0)
+    # Make sure the user wants to descend into a directory
+    while IFS= read -u 3 -r -d '' dir; do
+        descend_prompt="Do you want to descend into the directory "${dir}"?"
+        if confirm "\${descend_prompt}"; then
+            check_files "\${dir}/"
+        fi
+    done 3< <(find "${filepath}"* -maxdepth 0 -type d -print0)
+}
+
+### Main script ###
+# Create the ignore file if it doesn't already exist
+if [ ! -f ~/.cleanup-ignore.txt ]; then
+    echo "" > ~/.cleanup-ignore.txt
+fi
+
+check_files "./"
